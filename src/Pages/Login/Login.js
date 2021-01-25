2c01c3d0c98f6f3c4cad17ff794f3a30372f5ff0
문규찬 <lazychan90@gmail.com>
2021-01-26 02:15:44 +0900

    market-ssua 로그인 페이지

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,57 @@
 import React from "react";
-import "./Login.scss"
+import "./Login.scss";
 
-class Login extends React.Component{
-  render(){
-    return(
-    <div>
-    </div>
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleLoginInfo = (e) => {
+    const { id, value } = e.target;
+    this.setState({ [id]: value });
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <section>
+          <h3>로그인</h3>
+          <div className="login_input">
+            <input
+              id="email"
+              type="text"
+              placeholder="아이디를 입력해주세요"
+              value={this.state.email}
+              onChange={this.handleLoginInfo}
+            />
+            <input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              value={this.state.password}
+              onChange={this.handleLoginInfo}
+            />
+          </div>
+          <div className="mid_content">
+            <div className="login_check">
+              <input type="checkbox" />
+              <span>보안접속</span>
+            </div>
+            <div className="find">
+              <span>아이디 찾기</span>
+              <span>비밀번호 찾기</span>
+            </div>
+          </div>
+          <div className="login_button">
+            <button>로그인</button>
+            <button>회원가입</button>
+          </div>
+        </section>
+      </div>
     );
   }
 }
